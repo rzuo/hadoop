4a0ea4fbe2b6221c15681d11bf608e748bbf72e6
rzuo <rongguo.zuo@gmail.com>
2023-06-09 12:10:44 +0800

    1. upgrade zookeeper to 3.6.4; 2. add build shell script for easy compile.

---
+++ my-build-cmd.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+export JAVA8_HOME=/usr/local/java8
+export JAVA_HOME=${JAVA8_HOME}
+
+CONTINUE_MOD=$1
+
+DATE_STRING=`date +%Y-%m-%d`
+BUILD_PARAMETERS=""
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  BUILD_PARAMETERS="-Dsnappy.prefix=x \
+                    -Dbundle.snappy=true \
+                    -Dsnappy.lib=/usr/lib64 \
+                    -Pdist \
+                    -Pnative \
+                    -Psrc \
+                    -Pyarn-ui \
+                    -Dtar \
+                    -Dzookeeper.version=3.6.4 \
+                    -Dleveldbjni.group=org.fusesource.leveldbjni \
+                    -DskipTests \
+                    -DskipITs \
+                    clean install"
+else
+  BUILD_PARAMETERS="-Dsnappy.prefix=x \
+                    -Dbundle.snappy=true \
+                    -Dsnappy.lib=/usr/lib64 \
+                    -Pdist \
+                    -Pnative \
+                    -Psrc \
+                    -Pyarn-ui \
+                    -Dtar \
+                    -Dzookeeper.version=3.6.4 \
+                    -Dleveldbjni.group=org.fusesource.leveldbjni \
+                    -DskipTests \
+                    -DskipITs \
+                    clean install"
+fi
+
+mkdir -p my-build-logs || true
+if [ "x${CONTINUE_MOD}" == "x" ]; then
+  echo "building from 1st module"
+  echo "-----------------------------start of new building----------------------------------------" | tee -a my-build-logs/build-${DATE_STRING}.log
+  mvn -s ~/.m2/settings.xml -gs ~/.m2/settings.xml ${BUILD_PARAMETERS} 2>&1 | tee -a my-build-logs/build-${DATE_STRING}.log
+else
+  echo "building from module: ${CONTINUE_MOD}"
+  echo "------------------------continue building from ${CONTINUE_MOD}---------------------------------" | tee -a my-build-logs/build-${DATE_STRING}.log
+  mvn -s ~/.m2/settings.xml -gs ~/.m2/settings.xml ${BUILD_PARAMETERS} -rf :${CONTINUE_MOD} 2>&1 | tee -a my-build-logs/build-${DATE_STRING}.log
+fi
